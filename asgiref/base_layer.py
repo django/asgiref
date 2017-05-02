b741685af1e63f16b52040cd0b3fc790d44a1512
Andrew Godwin <andrew@aeracode.org>
2017-05-02 10:22:51 -0700

    Clarify meaning of "alphanumerics"

--- asgiref/base_layer.py
+++ asgiref/base_layer.py
@@ -102,7 +102,7 @@ class BaseChannelLayer(object):
 
     channel_name_regex = re.compile(r"^[a-zA-Z\d\-_.]+((\?|\!)[\d\w\-_.]*)?$")
     group_name_regex = re.compile(r"^[a-zA-Z\d\-_.]+$")
-    invalid_name_error = "{} name must be a valid unicode string containing only alphanumerics, hyphens, underscores, or periods."
+    invalid_name_error = "{} name must be a valid unicode string containing only ASCII alphanumerics, hyphens, underscores, or periods."
 
     def valid_channel_name(self, name, receive=False):
         if self.match_type_and_length(name):
@@ -113,13 +113,13 @@ class BaseChannelLayer(object):
                 elif "!" in name and not name.endswith("!") and receive:
                     raise TypeError("Process-local channel names in receive() must end at the !")
                 return True
-        raise TypeError("Channel name must be a valid unicode string containing only alphanumerics, hyphens, or periods, not '{}'.".format(name))
+        raise TypeError("Channel name must be a valid unicode string containing only ASCII alphanumerics, hyphens, or periods, not '{}'.".format(name))
 
     def valid_group_name(self, name):
         if self.match_type_and_length(name):
             if bool(self.group_name_regex.match(name)):
                 return True
-        raise TypeError("Group name must be a valid unicode string containing only alphanumerics, hyphens, or periods.")
+        raise TypeError("Group name must be a valid unicode string containing only ASCII alphanumerics, hyphens, or periods.")
 
     def valid_channel_names(self, names, receive=False):
         _non_empty_list = True if names else False
