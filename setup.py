4c2984ceb68a1832eeb0974f7d3a81e7fe3454d3
David Sanders <dsanders11@ucsbalum.com>
2017-07-16 21:15:30 -0700

    Add classifiers to setup.py (#21)

--- setup.py
+++ setup.py
@@ -25,4 +25,19 @@ setup(
     entry_points={'console_scripts': [
         'asgiref_benchmark = asgiref.benchmark:Benchmarker.cli',
     ]},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )
