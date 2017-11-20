51511b593e15a486ee69746749606e68f64e1ebc
Andrew Godwin <andrew@aeracode.org>
2017-11-19 19:10:15 -0800

    Remove double defined function

--- asgiref/sync.py
+++ asgiref/sync.py
@@ -3,19 +3,6 @@ import functools
 from concurrent.futures import ThreadPoolExecutor, Future
 
 
-def callable_application(func):
-    """
-    Function/method decorator that turns an async callable that takes
-    (conntype, receive, send) into an ASGI application by doing the
-    double-callable step internally.
-    """
-    def outer(conntype):
-        async def inner(receive, send):
-            await func(conntype, receive, send)
-        return inner
-    return outer
-
-
 class AsyncToSync:
     """
     Utility class which turns an awaitable that only works on the thread with
