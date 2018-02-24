7ca0ca0346433bd84b669999278247b684be826e
Andrew Godwin <andrew@aeracode.org>
2018-02-23 19:30:11 -0800

    Re-add classifiers

--- setup.py
+++ setup.py
@@ -28,4 +28,19 @@ setup(
     install_requires=[
         'async-timeout~=2.0',
     ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )
