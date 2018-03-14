ca001d16f2fec0554f8b5b7fb1dd3e052b4ce79c
Daniel <33256939+dgilge@users.noreply.github.com>
2018-03-14 07:53:31 +0100

    Simplify test

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,37 +1,13 @@
-import math
-import time
-
 import pytest
 
 from asgiref.testing import ApplicationCommunicator
 from asgiref.wsgi import WsgiToAsgi
 
 
-@pytest.mark.parametrize(
-    "event, timeout, interval, duration, tolerance, expected", [
-        # No event
-        # Standard values
-        (False, 0.1, 0.01, 0.1, 0.03, True),
-        # interval > timeout (interval has precedence over timeout)
-        (False, 0.1, 1, 1, 0.03, True),
-        # timeout % interval > 0 (interval cycle completes)
-        (False, 1, 0.6, 1.2, 0.03, True),
-
-        # Events
-        # Standard values
-        (True, 0.1, 0.01, 0.01, 0.003, False),
-        # No delay
-        (True, 0, 0.01, 0, 0.003, True),
-        # No interval
-        (True, 0.1, 0, 0, 0.003, False),
-        # interval > timeout (interval has precedence over timeout)
-        (True, 0.1, 1, 1, 0.03, False),
-    ]
-)
 @pytest.mark.asyncio
-async def test_receive_nothing(event, timeout, interval, duration, tolerance, expected):
+async def test_receive_nothing():
     """
-    Tests ApplicationCommunicator.receive_nothing with different parameters.
+    Tests ApplicationCommunicator.receive_nothing to return the correct value.
     """
     # Get an ApplicationCommunicator instance
     def wsgi_application(environ, start_response):
@@ -47,21 +23,17 @@ async def test_receive_nothing(event, timeout, interval, duration, tolerance, ex
         "headers": [],
     })
 
-    if event:
-        await instance.send_input({
-            "type": "http.request",
-        })
+    # No event
+    assert await instance.receive_nothing() is True
 
-    start = time.monotonic()
-    result = await instance.receive_nothing(timeout=timeout, interval=interval)
-    end = time.monotonic()
-    assert result is expected
-    assert math.isclose(end - start, duration, abs_tol=tolerance)
-
-    if event:
-        await instance.receive_output()
-        assert await instance.receive_nothing() is False
-        await instance.receive_output()
-        assert await instance.receive_nothing() is False
-        await instance.receive_output()
-        assert await instance.receive_nothing(0.01) is True
+    # Events
+    await instance.send_input({
+        "type": "http.request",
+    })
+    assert await instance.receive_nothing() is False
+    await instance.receive_output()
+    assert await instance.receive_nothing() is False
+    await instance.receive_output()
+    assert await instance.receive_nothing() is False
+    await instance.receive_output()
+    assert await instance.receive_nothing(0.01) is True
