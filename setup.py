55459a6538bd2d89fc23c9ee6f7f4c49490a6574
Till Theato <theato@ttill.de>
2018-05-13 16:18:15 +0200

    Bump async-timeout version to 3.0 (#40)
    Resolves dependency conflict with aiohttp.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         ],
     },
     install_requires=[
-        'async-timeout~=2.0',
+        'async_timeout>=3.0,<4.0',
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
