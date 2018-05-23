2bd62c4b241340ca79292c3b9ef3ad87715e64c8
Andrew Godwin <andrewgodwin@eventbrite.com>
2018-05-23 14:12:22 +0200

    Fix version to include 2.0 for now (3.0 appears unfindable)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         ],
     },
     install_requires=[
-        'async_timeout~=3.0',
+        'async_timeout>=2.0,<4.0',
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
