308ac1563d2fddf9a31976d80d2555b8c3b4ee85
Carlton Gibson <carlton.gibson@noumenal.es>
2019-10-23 13:49:57 +0200

    Fixed #132 -- Set explicit loop policy for Windows & Python 3.8.

--- asgiref/__init__.py
+++ asgiref/__init__.py
@@ -1,1 +1,9 @@
+import asyncio
+import sys
+
 __version__ = "3.2.2"
+
+
+# Avoid issue #132 on Windows & Python 3.8.
+if sys.platform == "win32" and sys.version_info >= (3, 8, 0):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
