ff4f665b4322bf6c7ea4158af7577bdf0a088a0d
Adam Johnson <me@adamj.eu>
2019-11-04 18:52:41 +0000

    Declared python_requires in setup.py
    This will prevent installation on unsupported Python versions.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     zip_safe=False,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
+    python_requires=">=3.5",
     extras_require={
         "tests": [
             "pytest~=4.3.0",
