82dadf8e83bcf0158563431ddfe8fda750cec5af
Dev Aggarwal <devxpy@gmail.com>
2020-05-13 00:00:52 +0530

    Rename SyncToAsyncIterable to SyncIterableToAsync for consistency
    Format with black

--- asgiref/sync_iter.py
+++ asgiref/sync_iter.py
@@ -9,26 +9,24 @@ def sync_generator_fn_to_async(sync_generator_fn, impl, thread_sensitive=False):
 
     @wraps(sync_generator_fn)
     def wrapper(*args, **kwargs):
-
         async def get_sync_iterable():
             # the async generator function should return a sync generator.
             # since a generator is also an iterable, it is compatible with SyncToAsyncIterable
             return await async_generator_fn(*args, **kwargs)
 
-        return SyncToAsyncIterable(get_sync_iterable, impl, thread_sensitive)
+        return SyncIterableToAsync(get_sync_iterable, impl, thread_sensitive)
 
     return wrapper
 
 
 def sync_iterable_to_async(sync_iterable, impl, thread_sensitive=False):
-
     async def get_sync_iterable():
         return sync_iterable
 
-    return SyncToAsyncIterable(get_sync_iterable, impl, thread_sensitive)
+    return SyncIterableToAsync(get_sync_iterable, impl, thread_sensitive)
 
 
-class SyncToAsyncIterable():
+class SyncIterableToAsync:
     def __init__(
         self,
         get_sync_iterable: typing.Callable[[], typing.Awaitable[typing.Iterable]],
@@ -47,7 +45,7 @@ class SyncToAsyncIterable():
         return self
 
     async def __anext__(self):
-        if not hasattr(self, 'sync_iterator'):
+        if not hasattr(self, "sync_iterator"):
             # one-time setup of the internal iterator
             sync_iterable = await self.get_sync_iterable()
             self.sync_iterator = await self.iter_async(sync_iterable)
