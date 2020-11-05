49598d17e973e4ba4b5d09528d92a6f87ac139fe
Carlton Gibson <carlton.gibson@noumenal.es>
2020-11-05 20:35:33 +0100

    Added test for StatelessServer using ASGI v3 application.
    Refs #209

---
+++ tests/test_server.py
@@ -0,0 +1,11 @@
+from asgiref.server import StatelessServer
+
+
+def test_stateless_server():
+    """StatlessServer can be instantiated with an ASGI 3 application."""
+
+    async def app(scope, receive, send):
+        pass
+
+    server = StatelessServer(app)
+    server.get_or_create_application_instance("scope_id", {})
