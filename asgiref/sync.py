d35966bf3058b358a3c84da129f9aa2d6ee62a13
Thomas Grainger <tagrain@gmail.com>
2021-02-04 18:21:36 +0000

    Typechecked annotations (and droped support for python 3.5)
    Also adds mypy to enforce typing

--- asgiref/sync.py
+++ asgiref/sync.py
@@ -1,4 +1,3 @@
-import asyncio
 import asyncio.coroutines
 import functools
 import os
@@ -6,13 +5,14 @@ import sys
 import threading
 import weakref
 from concurrent.futures import Future, ThreadPoolExecutor
+from typing import Dict
 
 from .current_thread_executor import CurrentThreadExecutor
 from .local import Local
 
-try:
-    import contextvars  # Python 3.7+ only.
-except ImportError:
+if sys.version_info >= (3, 7):
+    import contextvars
+else:
     contextvars = None
 
 
@@ -94,7 +94,7 @@ class AsyncToSync:
     """
 
     # Maps launched Tasks to the threads that launched them (for locals impl)
-    launch_map = {}
+    launch_map: "Dict[asyncio.Task[object], threading.Thread]" = {}
 
     # Keeps track of which CurrentThreadExecutor to use. This uses an asgiref
     # Local, not a threadlocal, so that tasks can work out what their parent used.
@@ -301,7 +301,7 @@ class SyncToAsync:
         )
 
     # Maps launched threads to the coroutines that spawned them
-    launch_map = {}
+    launch_map: "Dict[threading.Thread, asyncio.Task[object]]" = {}
 
     # Storage for main event loop references
     threadlocal = threading.local()
@@ -317,7 +317,9 @@ class SyncToAsync:
 
     # Maintaining a weak reference to the context ensures that thread pools are
     # erased once the context goes out of scope. This terminates the thread pool.
-    context_to_thread_executor = weakref.WeakKeyDictionary()
+    context_to_thread_executor: "weakref.WeakKeyDictionary[object, ThreadPoolExecutor]" = (
+        weakref.WeakKeyDictionary()
+    )
 
     def __init__(self, func, thread_sensitive=True):
         self.func = func
