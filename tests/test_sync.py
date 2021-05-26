8b49f36b4a516529f280cb83204f77d838cef3b3
shughes <shughes@nomnomdata.com>
2021-05-25 21:11:03 -0700

    closes #249

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -669,3 +669,26 @@ async def test_sync_to_async_uses_executor():
         thread_sensitive=True,
         executor=custom_executor,
     )
+
+
+def test_sync_to_async_deadlock_raises():
+    def db_write():
+        pass
+
+    async def io_task():
+        await sync_to_async(db_write)()
+
+    async def do_io_tasks():
+        t = asyncio.create_task(io_task())
+        await t
+        # await asyncio.gather(io_task()) # Also deadlocks
+        # await io_task() # Works
+
+    def view():
+        async_to_sync(do_io_tasks)()
+
+    async def server_entry():
+        await sync_to_async(view)()
+
+    with pytest.raises(RuntimeError):
+        asyncio.run(server_entry())
