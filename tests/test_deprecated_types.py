c527b6e34484408dd1d856d8ca664fe025022afa
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-06-26 12:59:56 +0200

    use same pattern on both tests

--- tests/test_deprecated_types.py
+++ tests/test_deprecated_types.py
@@ -7,10 +7,10 @@ from asgiref import typing
 
 @pytest.mark.parametrize("deprecated_type", typing.__deprecated__.keys())
 def test_deprecated_types(deprecated_type: str) -> None:
-    with pytest.deprecated_call() as warnings:
+    with pytest.warns(DeprecationWarning) as record:
         getattr(importlib.import_module("asgiref.typing"), deprecated_type)
-        assert len(warnings.list) == 1
-        assert deprecated_type in str(warnings.list[0])
+        assert len(record) == 1
+        assert deprecated_type in str(record.list[0])
 
 
 @pytest.mark.parametrize("available_type", typing.__all__)
