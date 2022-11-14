8b76e05b80a944aa9f3527072120db635ef9d31e
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-11-14 11:35:16 -0600

    Tweak conf.py to fix Sphinx warnings (#355)

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ release = "3.0"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -95,7 +95,7 @@ html_theme = "default"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path: List[str] = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
