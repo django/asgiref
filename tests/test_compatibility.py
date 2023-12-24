3c7bd107445efa7ceac9d9e254eda4590314fad4
davitacols <98448386+davitacols@users.noreply.github.com>
2023-12-24 13:33:35 +0100

    removed unused arguments

--- tests/test_compatibility.py
+++ tests/test_compatibility.py
@@ -1,10 +1,16 @@
+"""
+Tests for asgiref compatibility functions.
+"""
+
 import pytest
 from asgiref.compatibility import double_to_single_callable, is_double_callable
 from asgiref.testing import ApplicationCommunicator
 
 
-def double_application_function(scope):
-    """A nested function-based double-callable application."""
+async def double_application_function(scope):
+    """
+    A nested function-based double-callable application.
+    """
     async def inner(receive, send):
         message = await receive()
         await send({"scope": scope["value"], "message": message["value"]})
@@ -12,7 +18,9 @@ def double_application_function(scope):
 
 
 class DoubleApplicationClass:
-    """A classic class-based double-callable application."""
+    """
+    A classic class-based double-callable application.
+    """
     def __init__(self, scope):
         pass
 
@@ -21,7 +29,9 @@ class DoubleApplicationClass:
 
 
 class DoubleApplicationClassNestedFunction:
-    """A function closure inside a class."""
+    """
+    A function closure inside a class.
+    """
     def __init__(self):
         pass
 
@@ -32,22 +42,28 @@ class DoubleApplicationClassNestedFunction:
 
 
 async def single_application_function(scope, receive, send):
-    """A single-function single-callable application."""
+    """
+    A single-function single-callable application.
+    """
     pass
 
 
 class SingleApplicationClass:
-    """A single-callable class."""
+    """
+    A single-callable class.
+    """
     def __init__(self):
         pass
 
-    async def __call__(self, scope, receive, send):
+    async def __call__(self, receive, send):
         pass
 
 
 @pytest.mark.asyncio
 async def test_is_double_callable():
-    """Test the behavior of is_double_callable function."""
+    """
+    Test the behavior of is_double_callable function.
+    """
     assert is_double_callable(double_application_function)
     assert is_double_callable(DoubleApplicationClass)
     assert is_double_callable(DoubleApplicationClassNestedFunction())
@@ -57,14 +73,18 @@ async def test_is_double_callable():
 
 @pytest.mark.asyncio
 async def test_double_to_single_callable():
-    """Test the behavior of double_to_single_callable function."""
+    """
+    Test the behavior of double_to_single_callable function.
+    """
     new_app = double_to_single_callable(double_application_function)
     assert not is_double_callable(new_app)
 
 
 @pytest.mark.asyncio
 async def test_double_to_single_communicator():
-    """Test the behavior of the new application using ApplicationCommunicator."""
+    """
+    Test the behavior of the new application using ApplicationCommunicator.
+    """
     new_app = double_to_single_callable(double_application_function)
     instance = ApplicationCommunicator(new_app, {"value": "woohoo"})
     await instance.send_input({"value": 42})
