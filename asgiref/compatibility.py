9fb1fbcb53abc3cb2c0ac366a0a5f7ef3448229c
davitacols <98448386+davitacols@users.noreply.github.com>
2023-12-27 14:31:34 +0100

    work in progress

--- asgiref/compatibility.py
+++ asgiref/compatibility.py
@@ -1,52 +1,45 @@
 import inspect
-from typing import Callable, Coroutine, Optional, Type, Any
+from typing import Any, Callable, Coroutine, Optional
 
 from .sync import iscoroutinefunction
 
-def is_double_callable(application: Callable[..., Optional[Callable[[], Coroutine[Any, Any, None]]]]) -> bool:
+
+def is_double_callable(
+    application: Callable[..., Optional[Callable[[], Coroutine[Any, Any, None]]]]
+) -> bool:
     """
-    Tests whether an application is a legacy-style (double-callable) application.
-
-    Parameters:
-    - `application`: The callable object to be tested.
-
-    Returns:
-    - `True` if the application is a double-callable, otherwise `False`.
+    Tests to see if an application is a legacy-style (double-callable) application.
     """
-
     # Look for a hint on the object first
-    if getattr(application, "_asgi_single_callable", False) or getattr(application, "_asgi_double_callable", False):
+    if getattr(application, "_asgi_single_callable", False):
+        return False
+    if getattr(application, "_asgi_double_callable", False):
         return True
     # Uninstantiated classes are double-callable
     if inspect.isclass(application):
         return True
     # Instantiated classes depend on their __call__
-    if hasattr(application, "__call__") and not iscoroutinefunction(application.__call__):
-        return True
-    # Non-classes are checked directly
+    if hasattr(application, "__call__"):
+        # We only check to see if its __call__ is a coroutine function -
+        # if it's not, it still might be a coroutine function itself.
+        if iscoroutinefunction(application.__call__):
+            return False
+    # Non-classes we just check directly
     return not iscoroutinefunction(application)
 
 
 def double_to_single_callable(
     application: Callable[..., Callable[[], Coroutine[Any, Any, None]]]
-) -> Callable[[dict, Callable[[], Any], Callable[[Any], None]], Coroutine[Any, Any, None]]:
+) -> Callable[
+    [dict[str, Any], Callable[[], Any], Callable[[Any], None]],
+    Coroutine[Any, Any, None],
+]:
     """
     Transforms a double-callable ASGI application into a single-callable one.
-
-    Parameters:
-    - `application`: The double-callable ASGI application.
-
-    Returns:
-    - A single-callable ASGI application.
-
-    Example:
-    ```python
-    single_callable_app = double_to_single_callable(double_callable_app)
-    ```
     """
 
     async def new_application(
-        scope: dict, receive: Callable[[], Any], send: Callable[[Any], None]
+        scope: dict[str, Any], receive: Callable[[], Any], send: Callable[[Any], None]
     ) -> None:
         instance = application(scope)
         await instance(receive, send)
@@ -56,23 +49,15 @@ def double_to_single_callable(
 
 def guarantee_single_callable(
     application: Callable[..., Optional[Callable[[], Coroutine[Any, Any, None]]]]
-) -> Callable[[dict, Callable[[], Any], Callable[[Any], None]], Coroutine[Any, Any, None]]:
+) -> Callable[
+    [dict[str, Any], Callable[[], Any], Callable[[Any], None]],
+    Coroutine[Any, Any, None],
+]:
     """
     Takes either a single- or double-callable application and always returns it
-    in single-callable style.
-
-    Parameters:
-    - `application`: The single- or double-callable ASGI application.
-
-    Returns:
-    - A single-callable ASGI application.
-
-    Example:
-    ```python
-    guaranteed_single_callable = guarantee_single_callable(some_callable_app)
-    ```
+    in single-callable style. Use this to add backward compatibility for ASGI
+    2.0 applications to your server/test harness/etc.
     """
-
     if is_double_callable(application):
         application = double_to_single_callable(application)
     return application
